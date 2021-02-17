9c89cadca65b766298cf39da81d8bca9131b08c4
SutandoTsukai181 <52977072+SutandoTsukai181@users.noreply.github.com>
2021-02-17 20:05:12 +0300

    Implement some file searching methods
    get_file_from_path does not check for duplicate files if par_root is false, it will simply pick the first file found in the par's file list.

--- src/par.py
+++ src/par.py
@@ -26,7 +26,53 @@ class File:
     data: bytearray
 
 
-class Folder:
+class Directory:
+    files: List[File]
+    folders: List['Folder']
+
+    def get_file(self, file_name: str) -> File:
+        """searches for the file in this directory.
+        if the directory is a par, searches in all files of the par."""
+
+        file = [f for f in self.files if f.name == file_name]
+        if len(file):
+            return file[0]
+        return None
+
+    def get_folder(self, folder_name: str) -> 'Folder':
+        """searches for the folder in this directory.
+        if the directory is a par, searches in all folders of the par."""
+
+        folder = [f for f in self.folders if f.name == folder_name]
+        if len(folder):
+            return folder[0]
+        return None
+
+    def __file_from_path(self, paths: List[str], file_name: str) -> File:
+        if len(paths):
+            folder = self.get_folder(paths.pop(0))
+            if folder:
+                return folder.__file_from_path(paths, file_name)
+        else:
+            return self.get_file(file_name)
+        return None
+
+    def get_file_from_path(self, path: str, par_root=False) -> File:
+        """path is the path to the file relative to the current directory (folder/par),
+        separated by forward slashes (/). if par_root is true, will start searching
+        inside the root folder (.) of the par.
+        """
+
+        paths = [p for p in path.split("/") if p != ""]
+        file_name = paths.pop()
+
+        if par_root and len(self.folders):
+            return self.folders[0].__file_from_path(paths, file_name)
+
+        return self.__file_from_path(paths, file_name)
+
+
+class Folder(Directory):
     name: str
 
     folder_count: int
@@ -36,12 +82,6 @@ class Folder:
 
     attributes: int
 
-    folders: List['Folder']
-    files: List[File]
 
-
-class Par:
+class Par(Directory):
     header: Header
-
-    folders: List[Folder]
-    files: List[File]
